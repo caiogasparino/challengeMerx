9627a2dede09778ca7e7f7bf83b66ae0630c157e
Caio Gasparino <gasparino.caio@gmail.com>
2022-09-27 19:56:31 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React from 'react'
 import './App.css'
-import AppBar from './components/AppBar'
 import { Route, Routes } from 'react-router-dom'
 import DataTable from './components/Table'
-import DataTableDetails from './components/Table'
 
 function App() {
   return (
     <div className="App">
-      <AppBar />
       <Routes>
         <Route path="/" element={<DataTable />} />
-        <Route path="/Table/:id" element={<DataTableDetails />} />
       </Routes>
     </div>
   )
--- src/components/Table.js
+++ src/components/Table.js
@@ -4,38 +4,62 @@ import React, { useMemo } from "react";
 import MaterialReactTable from "material-react-table";
 
 //Material-UI Imports
-import {
-    Box,
-    Button,
-    ListItemIcon,
-    MenuItem,
-    Typography,
-    TextField
-} from "@mui/material";
+import { Box, MenuItem, Typography, TextField } from "@mui/material";
 
 //Date Picker Imports
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-//Icons Imports
-import { AccountCircle, Send } from "@mui/icons-material";
-
 //Mock Data
 import data from "../dataTable";
 
-const Example = () => {
+const DataTable = () => {
     const columns = useMemo(
         () => [
             {
                 id: "transactions", //id used to define `group` column
-                header: "Transactions",
+                header: "ORDERS",
                 columns: [
+                    {
+                        accessorFn: (row) => `${row.status[0].description}`,
+                        accessorKey: "status",
+                        filterVariant: "range",
+                        header: "STATUS",
+                        size: 150,
+                        //custom conditional format and styling
+                        Cell: ({ cell }) => (
+                            <Box
+                                sx={(theme) => ({
+                                    backgroundColor:
+                                        (cell.getValue() === "Wrong Amount" &&
+                                            theme.palette.error.dark) ||
+                                        (cell.getValue() ===
+                                            "Payment Request" &&
+                                            theme.palette.warning.dark) ||
+                                        (cell.getValue() ===
+                                            "Payment Approved" &&
+                                            theme.palette.primary.dark) ||
+                                        (cell.getValue() === "Create a order" &&
+                                            theme.palette.success.light) ||
+                                        (cell.getValue() ===
+                                            "Electronic Invoice Sent" &&
+                                            theme.palette.secondary.dark),
+                                    borderRadius: "0.25rem",
+                                    color: "#fff",
+                                    maxWidth: "22ch",
+                                    p: ".50rem"
+                                })}
+                            >
+                                <Typography>{cell.getValue()}</Typography>
+                            </Box>
+                        )
+                    },
                     {
                         accessorFn: (row) => `${row.order}`, //accessorFn used to join multiple data into a single cell
                         id: "invoice", //id is still required when using accessorFn instead of accessorKey
                         header: "INVOICE",
-                        size: 250,
+                        size: 100,
                         Cell: ({ cell, row }) => (
                             <Box
                                 sx={{
@@ -49,11 +73,11 @@ const Example = () => {
                         )
                     },
                     {
-                        accessorFn: (row) => `${row.payment}`,
+                        accessorFn: (row) => `${row.payment.amount}`,
                         id: "payment", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
                         enableClickToCopy: true,
                         header: "BALANCE",
-                        size: 300,
+                        size: 100,
                         Cell: ({ cell, row }) => (
                             <Box
                                 sx={{
@@ -65,52 +89,10 @@ const Example = () => {
                                 <Typography>${cell.getValue()}</Typography>
                             </Box>
                         )
-                    }
-                ]
-            },
-            {
-                id: "id",
-                header: "Job Info",
-                columns: [
-                    {
-                        accessorKey: "salary",
-                        filterVariant: "range",
-                        header: "SALARY",
-                        size: 200,
-                        //custom conditional format and styling
-                        Cell: ({ cell }) => (
-                            <Box
-                                sx={(theme) => ({
-                                    backgroundColor:
-                                        cell.getValue() < 50_000
-                                            ? theme.palette.error.dark
-                                            : cell.getValue() >= 50_000 &&
-                                              cell.getValue() < 75_000
-                                            ? theme.palette.warning.dark
-                                            : theme.palette.success.dark,
-                                    borderRadius: "0.25rem",
-                                    color: "#fff",
-                                    maxWidth: "9ch",
-                                    p: "0.25rem"
-                                })}
-                            >
-                                {cell.getValue()?.toLocaleString?.("en-US", {
-                                    style: "currency",
-                                    currency: "USD",
-                                    minimumFractionDigits: 0,
-                                    maximumFractionDigits: 0
-                                })}
-                            </Box>
-                        )
                     },
                     {
-                        accessorKey: "jobTitle", //hey a simple column for once
-                        header: "Job Title",
-                        size: 350
-                    },
-
-                    {
-                        accessorFn: (row) => new Date(row.payment.amount), //convert to Date for sorting and filtering
+                        accessorFn: (row) =>
+                            new Date(`${row.payment["due-date"]}`), //convert to Date for sorting and filtering
                         id: "due-date",
                         header: "DUE DATE",
                         filterFn: "lessThanOrEqualTo",
@@ -141,6 +123,24 @@ const Example = () => {
                                 />
                             </LocalizationProvider>
                         )
+                    },
+                    {
+                        accessorFn: (row) => `${row.terms}`,
+                        id: "terms", //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+                        enableClickToCopy: true,
+                        header: "BEYOND TERMS",
+                        size: 50,
+                        Cell: ({ cell, row }) => (
+                            <Box
+                                sx={{
+                                    display: "flex",
+                                    alignItems: "center",
+                                    gap: "1rem"
+                                }}
+                            >
+                                <Typography>{cell.getValue()}</Typography>
+                            </Box>
+                        )
                     }
                 ]
             }
@@ -152,39 +152,11 @@ const Example = () => {
         <MaterialReactTable
             columns={columns}
             data={data}
-            enableColumnFilterModes
+            displayColumnDefOptions
             enableColumnOrdering
-            enableGrouping
-            enablePinning
             enableRowActions
-            enableRowSelection
-            initialState={{ showColumnFilters: true }}
+            initialState={{ showColumnFilters: false }}
             positionToolbarAlertBanner="bottom"
-            renderDetailPanel={({ row }) => (
-                <Box
-                    sx={{
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center"
-                    }}
-                >
-                    <img
-                        alt="avatar"
-                        height={200}
-                        src={row.original.avatar}
-                        loading="lazy"
-                        style={{ borderRadius: "50%" }}
-                    />
-                    <Box sx={{ textAlign: "center" }}>
-                        <Typography variant="h4">
-                            Signature Catch Phrase:
-                        </Typography>
-                        <Typography variant="h1">
-                            &quot;{row.original.signatureCatchPhrase}&quot;
-                        </Typography>
-                    </Box>
-                </Box>
-            )}
             renderRowActionMenuItems={({ closeMenu }) => [
                 <MenuItem
                     key={0}
@@ -194,84 +166,11 @@ const Example = () => {
                     }}
                     sx={{ m: 0 }}
                 >
-                    <ListItemIcon>
-                        <AccountCircle />
-                    </ListItemIcon>
-                    View Profile
-                </MenuItem>,
-                <MenuItem
-                    key={1}
-                    onClick={() => {
-                        // Send email logic...
-                        closeMenu();
-                    }}
-                    sx={{ m: 0 }}
-                >
-                    <ListItemIcon>
-                        <Send />
-                    </ListItemIcon>
-                    Send Email
+                    RENDER MODAL DETAILS
                 </MenuItem>
             ]}
-            renderTopToolbarCustomActions={({ table }) => {
-                const handleDeactivate = () => {
-                    table.getSelectedRowModel().flatRows.map((row) => {
-                        alert("deactivating " + row.getValue("name"));
-                    });
-                };
-
-                const handleActivate = () => {
-                    table.getSelectedRowModel().flatRows.map((row) => {
-                        alert("activating " + row.getValue("name"));
-                    });
-                };
-
-                const handleContact = () => {
-                    table.getSelectedRowModel().flatRows.map((row) => {
-                        alert("contact " + row.getValue("name"));
-                    });
-                };
-
-                return (
-                    <div style={{ display: "flex", gap: "0.5rem" }}>
-                        <Button
-                            color="error"
-                            disabled={
-                                table.getSelectedRowModel().flatRows.length ===
-                                0
-                            }
-                            onClick={handleDeactivate}
-                            variant="contained"
-                        >
-                            Deactivate
-                        </Button>
-                        <Button
-                            color="success"
-                            disabled={
-                                table.getSelectedRowModel().flatRows.length ===
-                                0
-                            }
-                            onClick={handleActivate}
-                            variant="contained"
-                        >
-                            Activate
-                        </Button>
-                        <Button
-                            color="info"
-                            disabled={
-                                table.getSelectedRowModel().flatRows.length ===
-                                0
-                            }
-                            onClick={handleContact}
-                            variant="contained"
-                        >
-                            Contact
-                        </Button>
-                    </div>
-                );
-            }}
         />
     );
 };
 
-export default Example;
+export default DataTable;
